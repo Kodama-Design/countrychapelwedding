1f0bfe93bc279b7bc5a8efd9f2330feca4fa675b
Robert Parker <rparker@kodama.design>
2017-10-23 00:10:56 -0700

    setting up the theme

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp         = require('gulp'),
+    concat       = require('gulp-concat'),
+    sass         = require('gulp-sass'),
+    autoprefix   = require('gulp-autoprefixer'),
+    uglify       = require('gulp-uglify'),
+    plumber      = require('gulp-plumber'),
+    rename       = require('gulp-rename'),
+    notify       = require('gulp-notify'),
+    watch        = require('gulp-watch');
+
+var scripts_src = [
+    'bower_components/foundation-sites/src/js/foundation.js',
+    'bower_components/fancybox/src/jquery.fancybox.js',
+    'bower_components/slick-carousel/slick/slick.js',
+    'src/scripts/app.js'
+];
+var scripts_dist = 'src/assets';
+var scss_src = [
+    'src/scss/**/*.scss'
+];
+var scss_paths = [
+    'bower_components/font-awesome/scss',
+    'bower_components/slick-carousel/slick',
+    'bower_components/foundation-sites/scss',
+    'bower_components/fancybox/src/',
+    'src/scss/**/*.scss'
+];
+var scss_dist = 'src/assets';
+
+var plumberErrorHandler = { errorHandler: notify.onError({
+    title: 'Gulp',
+    message: 'Error: <%= error.message %>'
+})};
+
+gulp.task('default', [
+    'scripts',
+    'scss',
+    'watch'
+]);
+
+gulp.task('scripts', function(){
+    // app
+    gulp.src(scripts_src)
+        .pipe(concat('app.js'))
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest(scripts_dist));
+});
+
+gulp.task('scss', function(){
+    // minified
+    gulp.src(scss_src).pipe(plumber(plumberErrorHandler))
+        .pipe(sass({
+            style:          "compressed",
+            includePaths:   scss_paths,
+            comments:       true,
+            sourceComments: false
+        }))
+        .pipe(autoprefix('last 4 version'))
+        .pipe(gulp.dest(scss_dist));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(scripts_src, ['scripts']);
+    gulp.watch(scss_src, ['scss']);
+});
